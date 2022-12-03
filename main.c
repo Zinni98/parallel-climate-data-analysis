2447ca33a57ba6429eb247308f575411d08fd8f6
Zinni98 <alessandro.zinni98@icloud.com>
2022-12-03 17:18:42 +0100

    Slice vector working

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <netcdf.h>
 
 /**
@@ -42,36 +43,31 @@ int get_var_ids(int ncid, char** var_names, int var_ids[], int count)
 
 int main(int argc, char**argv)
 {
-    char path[] = "/shares/HPC4DataScience/FESOM2/fesom.mesh.diag.nc";
+    char path[] = "/shares/HPC4DataScience/FESOM2/vnod.fesom.2010.nc";
     int ncid = get_file_id(path);
-    char* vars[] = {"nz", "nz1", "lon", "lat"};
+    char* vars[] = {"nz1", "time", "vnod"};
     int var_ids[3];
-    int status = get_var_ids(ncid, vars, var_ids, 4);
+    int status = get_var_ids(ncid, vars, var_ids, 3);
     
     if (status != NC_NOERR)
         printf("Error during var_id retrieval\n");
     
-    double lon[8852366];
-    status = nc_get_var_double(ncid, var_ids[2], &lon[0]);
+    const size_t startv[3] = {0, 0, 0};
+    const size_t countv[3] = {1, 1, 200};
+    const ptrdiff_t stridev[3] = {1, 1, 1};
+    float vnod[12][69][8852366];
+    status = nc_get_vars_float(ncid, var_ids[2], &startv[0], &countv[0], &stridev[0], &vnod[0][0][0]);
     if (status != NC_NOERR)
     {
         printf("Error during lon var retrieval\n");
         return 1;
     }
-    
-    double lat[8852366];
-    status = nc_get_var_double(ncid, var_ids[3], &lat[0]);
-    if (status != NC_NOERR)
-    {
-        printf("Error during lat var retrieval\n");
-        return 1;
-    }
     else
     {
-        printf("Printing first 1000 coordinates:");
-        for(int i=0; i + 321921 < 1000 + 321921; i++)
+        printf("Printing first 200 coordinates:\n");
+        for(int i=0; i<200; i++)
         {
-            printf("lat: %f; lon: %f\n", lat[i], lon[i]);
+            printf("vnod: %f\n", vnod[0][0][i]);
         }
     }
 

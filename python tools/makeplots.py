b9267b6b509db6c15cfde9184d0bff5c2b03126b
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2022-12-09 13:40:05 +0100

    Tailored on filler_file algorithm

--- python tools/makeplots.py
+++ python tools/makeplots.py
@@ -4,8 +4,9 @@ import pandas as pd
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-cols = ["bytes", "time", "rate"]
-df = pd.read_csv("input.txt", header=None, names=cols, skiprows=1)
+cols = ["# Processes", "time"]
+input_file = "./out.txt"
+df = pd.read_csv(input_file, header=None, names=cols, skiprows=1)
 
 print("Contents in csv file:\n", df)
 print("Indexes : ", df.index)
@@ -13,11 +14,11 @@ print("Indexes : ", df.columns)
 print("Indexes : ", df.head())
 
 _, ax = plt.subplots()
-ax.plot(df["bytes"], df["time"], "-r", label="Exec Times")
-ax.plot(df["bytes"], df["rate"], "-b", label="Rate")
+ax.plot(df[cols[0]], df[cols[1]], "-r", label="Exec Times")
+# ax.plot(df["bytes"], df["rate"], "-b", label="Rate")
 ax.legend(frameon=False, loc="upper center", ncol=2)
 ax.grid()
-plt.xlabel("Bytes sent")
-plt.ylabel("Times / Rates")
+plt.xlabel("Number of processes")
+plt.ylabel("Times")
 plt.title("Benchmark")
 plt.show()

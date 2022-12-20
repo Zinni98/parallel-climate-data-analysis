8317845349a51f298f743c65639c447483f5ecc7
MatteoGuglielmi-tech <matteo.guglielmi.ma.gu@gmail.com>
2022-12-20 13:49:44 +0100

    Added c implementation

---
+++ c-implementation/parallel_max.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <netcdf.h>
+#include <stdint.h>
+#include <mpi.h>
+#include <math.h>
+#include "utils.h"
+
+
+int main(int argc, char**argv){
+
+    /**
+     * Note that the number of processes should be at least 6
+     * Otherwise it doesn't work when all nodes of the mesh
+     * are used.
+    */
+    int comm_sz, rank, cols;
+    int counts[comm_sz], displace[comm_sz];
+    float vnod[TIME][DEPTH][NODE2];
+
+    MPI_Init(NULL,NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if(rank==0)
+    {
+        printf("\n============= Parallel =============\n");
+        int status = 0;
+        const char* vars[] = {"nz1", "time", "vnod"};
+        int var_ids[3];
+        const int startv[3] = {0, 0, 0};
+        const int countv[3] = {TIME, DEPTH, NODE2};
+        const int stridev[3] = {1, 1, 1};
+        // get file id
+        int ncid = get_file_id(PATH2VNODE);
+        // get var ids
+        get_var_ids(ncid, vars, var_ids, 3);
+        status = read_velocity(ncid, vars, vars[2], startv, countv, stridev, vnod);
+        if(status){
+            printf("Error while retrieving vnod data\n");
+        }
+        printf("\n");
+        /* at this point process 0 should have read the velocity matrix */ 
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    double start = MPI_Wtime();
+
+    cols = NODE2/comm_sz;
+    if(rank == comm_sz - 1)
+    {
+        cols = NODE2 - (comm_sz - 1) * cols;
+    }
+
+    MPI_Datatype vcol, vcoltype;
+    if(rank == 0)
+    {
+        MPI_Type_vector(TIME*DEPTH, 1, NODE2, MPI_FLOAT, &vcol);
+        MPI_Type_commit(&vcol);
+        MPI_Type_create_resized(vcol, 0, sizeof(float), &vcoltype);
+        MPI_Type_commit(&vcoltype);
+    }
+
+    for(int i=0; i<comm_sz; i++)
+    {
+        if(i == comm_sz - 1)
+        {    
+            counts[i] = NODE2 - (comm_sz - 1) * cols;
+        }
+        else
+            counts[i] = cols;
+    }
+    for(int i=0; i<comm_sz; i++) displace[i] = i*cols;
+    float local_vnod[cols][TIME][DEPTH];
+    int local_n = TIME*DEPTH*cols;
+    MPI_Scatterv(vnod, counts, displace, vcoltype, local_vnod, local_n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    float local_max[cols][TIME];
+    compute_inverted_max(TIME, DEPTH, cols, local_vnod, local_max);
+
+    float max[TIME][NODE2];
+
+    MPI_Datatype recv, recv_type;
+    //(count, block_size, step)
+    MPI_Type_vector(TIME, 1, NODE2, MPI_FLOAT, &recv);
+    MPI_Type_commit(&recv);
+    MPI_Type_create_resized(recv, 0, sizeof(float), &recv_type);
+    MPI_Type_commit(&recv_type);
+
+    int recv_counts[comm_sz];
+    for(int i=0; i<comm_sz; i++)
+    {
+        if(i == comm_sz - 1)
+        {
+            recv_counts[i] = TIME * (NODE2 - (comm_sz - 1) * cols);
+        }
+        else
+            recv_counts[i] = cols*TIME;
+    }
+    int recv_displace[comm_sz];
+    for(int i=0; i<comm_sz; i++) recv_displace[i] = i*cols;
+
+    MPI_Gatherv(local_max, cols*TIME, MPI_FLOAT, max, recv_counts, recv_displace, recv_type, 0, MPI_COMM_WORLD);
+
+    double end = MPI_Wtime();
+    double tot= end - start;
+    /* if(rank == 0)
+    {
+        for(int i=0; i<TIME; i++)
+        {
+            for(int j=0; j<NODE2; j++)
+                printf("%f ", max[i][j]);
+            printf("\n");
+        }
+    } */
+
+    if(rank == 0)
+    {
+        printf("proc,time\n");
+        printf("%d, %f", comm_sz, tot);
+    }
+    // MPI_Barrier(MPI_COMM_WORLD);
+
+    if(rank == 0)
+    {
+        MPI_Type_free(&vcol);
+        MPI_Type_free(&vcoltype);
+    }
+    MPI_Type_free(&recv);
+    MPI_Type_free(&recv_type);
+    MPI_Finalize();
+
+    return 0;
+
+}
---
+++ utils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// imports
+#include <stdbool.h>
+
+// constants
+#define TIME 12
+#define DEPTH 69
+#define NODE2 8852364
+#define NUM_VARS 3
+
+#define PATH2UNODE "/shares/HPC4DataScience/FESOM2/unod.fesom.2010.nc"
+#define PATH2VNODE "/shares/HPC4DataScience/FESOM2/vnod.fesom.2010.nc"
+
+
+int get_file_id(const char* filename);
+void get_var_ids(int ncid, const char *var_names[NUM_VARS], int var_ids[], int count);
+int read_velocity(int ncid, const char *options[NUM_VARS], const char* var_name , const int *start_idxs, const int *count_idxs, 
+                const int *stride_steps, float target_buffer[TIME][DEPTH][NODE2]);
+bool check_var(const char *options[NUM_VARS], const char *var_name);
+int ceiling(int a, int b);
+void pop_max_matrix(float buffer[TIME][NODE2], int rows, int cols);
+void counts_and_displace(int *counts, int *displace, int comm_sz, int cols, char flag);
+//void compute_max(int dim1, int dim2, int dim3, float local_velocity[dim3][dim1][dim2], float local_max[dim1][dim3]);
+
+float** compute_maximum(const int var_idx, float (*matrix)[DEPTH][NODE2], int *dimension);
+
+void compute_norm(float vnode_mat[TIME][DEPTH][NODE2], float unode_mat[TIME][DEPTH][NODE2], float target_matrix[TIME][DEPTH][NODE2]);
+
+void compute_inverted_max(int dim1, int dim2, int dim3, float local_velocity[dim3][dim1][dim2], float local_max[dim3][dim1]);

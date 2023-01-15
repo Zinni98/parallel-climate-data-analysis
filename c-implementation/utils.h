b7af55cdd4b3a0d8d8e931364681246490de5b8e
Matteo <78434994+MatteoGuglielmi-tech@users.noreply.github.com>
2023-01-15 16:22:59 +0100

    ⏫ Removed useless function

--- c-implementation/utils.h
+++ c-implementation/utils.h
@@ -21,7 +21,6 @@ bool check_var(const char *options[NUM_VARS], const char *var_name);
 int check_status(int *status, const char* var_name);
 void pop_max_matrix(float buffer[TIME][NODE2], int rows, int cols);
 void counts_and_displace(int *counts, int *displace, int comm_sz, int cols, char flag);
-//void compute_max(int dim1, int dim2, int dim3, float local_velocity[dim3][dim1][dim2], float local_max[dim1][dim3]);
 float** compute_maximum(const int var_idx, float (*matrix)[DEPTH][NODE2], int *dimension);
 void compute_norm(float vnode_mat[TIME][DEPTH][NODE2], float unode_mat[TIME][DEPTH][NODE2], float target_matrix[TIME][DEPTH][NODE2]);
 void compute_inverted_max(int dim1, int dim2, int dim3, float local_velocity[dim3][dim1][dim2], float local_max[dim3][dim1]);

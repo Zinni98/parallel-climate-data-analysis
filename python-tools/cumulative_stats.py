b3f410a73adb6579bf7255ab6b21b720d537fe4e
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2023-01-15 23:25:27 +0100

    🔥 Removed cumulative stats unit test
    Removed unit test code to test cumulative stats computation

--- python-tools/cumulative_stats.py
+++ python-tools/cumulative_stats.py
@@ -15,14 +15,3 @@ def cumulative_stats(data: pd.DataFrame) -> pd.DataFrame:
     df: pd.DataFrame = data.copy()
     df = data.groupby(["# Processes"], as_index=False).mean()
     return df
-
-
-# df = pd.DataFrame(
-#    {
-#        "# Processes": [1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 6, 6, 7, 8, 9, 10],
-#        "Time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-#    }
-# )
-# print(df)
-# df = cumulative_stats(df)
-# print(df)
